f3f9d804b3b73f61f0dba2d95d63857b3308c6d8
Viraj Waim <virajwaim123@gmail.com>
2025-01-23 12:42:57 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "a35ec39b-a653-486c-b845-bd2c648124ed";
+export const MODAL_CLOSE_SEC = 2.5;
